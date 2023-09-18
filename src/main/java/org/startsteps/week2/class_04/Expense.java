cc316ff6fc64c65e450d89a7711a0f5c5375521d
Musie <musie.kebede@tier.app>
2023-09-18 14:58:56 +0200

    add solutions

--- src/main/java/org/startsteps/week2/class_04/Expense.java
+++ src/main/java/org/startsteps/week2/class_04/Expense.java
@@ -2,17 +2,40 @@ package org.startsteps.week2.class_04;
 
 public class Expense {
 
-//    Expense Categories:
-//    [Groceries, Rent, Utilities, Transportation, Entertainment, Savings] - []
-//     - 6 categories 3 * 6
+    //    Expense Categories:
+    //    [Groceries, Rent, Utilities, Transportation, Entertainment, Savings] - []
+    //     - 6 categories 3 * 6
 
-     //100, 290, 839, 282, 374, 283 // july  = [][]
+    // 100, 290, 839, 282, 374, 283 // july  = [][]
     // 100, 290, 839, 282, 374, 283 // august
     // 100, 290, 839, 282, 374, 283 // september
 
 
     // Total expense for the last 3 months
     // output: 2000
-            //   1000
-             //  1500
+    //  1000
+    //  1500
+
+
+    // 2492
+    public static void main(String[] args) {
+        String[] expenseCategories = {"Rent", "Groceries", "Utilities", "Transportation", "Entertainment", "Savings"}; //6
+        String[] months = {"January", "February", "March"};
+
+        int[][] expenses = {
+                {1000, 290, 282, 300, 500, 120},// Jan
+                {1000, 290, 282, 300, 500, 120}, // Feb
+                {1000, 290, 282, 300, 500, 120}  // Mar
+
+        };
+
+        for (int i = 0; i < expenses.length; i++) {
+            // total of expense calculation
+            int totalExpense = 0;
+            for (int j = 0; j < expenses[i].length; j++) {
+                totalExpense += expenses[i][j];
+            }
+            System.out.println(months[i] + "Total Expenses " + totalExpense + "€");
+        }
+    }
 }
