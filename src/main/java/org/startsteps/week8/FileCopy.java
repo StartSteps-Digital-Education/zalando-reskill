5f9ec4ff2134f39b7e649f75200c67f0abde087b
Musie <musie.kebede@tier.app>
2023-10-24 18:07:56 +0200

    week 08 day 2

---
+++ src/main/java/org/startsteps/week8/FileCopy.java
@@ -0,0 +1,19 @@
+package org.startsteps.week8;
+
+public class FileCopy {
+    // Byte streams (bytes 0/1)
+    // FileInputStream and FileOutputStream => read and write from files
+    // ByteArrayInputStream and ByteArrayOutputStream => read and write from memory
+    // DataInputStream and DataOutputStream => read and write from primitive data types
+    // BufferedInputStream and BufferedOutputStream => read and write buffer
+
+    // Character streams (text)
+    // FileReader and FileWriter => used to read and write from files
+    // CharArrayReader and CharArrayWriter => used to read / write from memory
+    // BufferedReader and BufferedWriter => for buffered data
+    // InputStreamReader and InputStreamWriter => byte and character streams
+
+
+    // Note: Buffered reader enables to minimize I/O operations in reading chunks
+
+}
