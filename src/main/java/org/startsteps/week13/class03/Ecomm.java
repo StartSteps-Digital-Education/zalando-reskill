124a45a4f97198e8fa2da396334fea5b51a2ccb5
MenilV <menil.vukovic@gmail.com>
2023-11-29 17:41:13 +0100

    Add exercises for week 13 day 3

---
+++ src/main/java/org/startsteps/week13/class03/Ecomm.java
@@ -0,0 +1,9 @@
+package org.startsteps.week13.class03;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class Ecomm extends ResourceConfig {
+    public Ecomm() {
+        packages("org.startsteps.week13.class03");
+    }
+}
